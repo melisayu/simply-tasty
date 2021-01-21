e5f9874dc1dd8c23fe4beb76a4370d42d6035042
melisayu <melisayuliakristin@gmail.com>
2021-01-21 17:29:48 +0100

    Define Scroll Behavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -44,7 +44,10 @@ const routes: Array<RouteConfig> = [
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
 
 export default router
